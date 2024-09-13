0af4e07413b3ab07df6cbd9622346582f1786d95
Muhammad Umer Khan <90978869+umerkhanuk@users.noreply.github.com>
2024-09-13 21:14:23 +0500

    first
    manually

---
+++ main.js
@@ -0,0 +1,28 @@
+let button = document.getElementById("myButton");
+let div = document.getElementById("skills");
+function showskills() {
+    div.style.display = div.style.display === 'none' ? 'block' : 'none';
+}
+button.addEventListener("click", showskills);
+export {};
+// let p1 = document.getElementById("pname") as HTMLInputElement;
+// let quan = document.getElementById("quan") as HTMLInputElement;
+// let price = document.getElementById("price") as HTMLInputElement;
+// let printButton = document.getElementById("printbtn") as HTMLButtonElement;
+// let printValue = document.getElementById("data") as HTMLOutputElement;
+// let printValue2 = document.getElementById("data2") as HTMLOutputElement;
+// let printValue3 = document.getElementById("data3") as HTMLOutputElement;
+// function addproduct():void{
+//     const product = p1.value;
+//     const qunatity = quan.value;
+//     const pri = price.value;
+//     let pvalue = printValue.textContent = product;
+//     let qvalue = printValue2.textContent = qunatity;
+//     let privalue = printValue3.textContent = pri;
+//     let save:string[] = []
+//     save.push(pvalue);
+//     save.push(qvalue);
+//     save.push(privalue)
+//     console.log(save);
+// }
+// printButton.addEventListener("click",addproduct)
